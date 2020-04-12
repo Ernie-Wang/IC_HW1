45912498378d2d27dfc95e635dab8b3eca07db63
NekoSaiKou <ricky0711.yang@gmail.com>
2020-04-13 00:34:33 +0800

    Fix: Change misused preserved keyword 'sum' to other

--- benchmark/F6.py
+++ benchmark/F6.py
@@ -2,15 +2,15 @@ import math
 import numpy as np
 
 def F_6(X):
-    sum = 0
+    tmp = 0
     for x in X:
-        sum = sum + math.pow( math.floor(x+0.5), 2)
-    return sum
+        tmp = tmp + math.pow( math.floor(x+0.5), 2)
+    return tmp
 
 if __name__ == '__main__':
     # X = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,])
     X = np.arange(10)
     X = X * 0.3
     print(X)
-    sum = F_6(X)
-    print(sum)
+    result = F_6(X)
+    print(result)
--- benchmark/F7.py
+++ benchmark/F7.py
@@ -3,18 +3,18 @@ import random
 import numpy as np
 
 def F_7(X):
-    sum = 0
+    tmp = 0
     for i, x in enumerate(X):
         rand = 1
         while rand == 1:
             rand = random.random()
-        sum = sum + math.pow(x, 4) * (i + 1) + rand
-    return sum
+        tmp = tmp + math.pow(x, 4) * (i + 1) + rand
+    return tmp
 
 if __name__ == '__main__':
     X = np.array([1, 1, 1, 1, 1])
     # X = np.arange(10)
     # X = X * 0.3
     print(X)
-    sum = F_7(X)
-    print(sum)
+    result = F_7(X)
+    print(result)
